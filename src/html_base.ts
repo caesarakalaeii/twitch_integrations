4f334b9ea4025c0ac1126e3948f2d612d6aeda8b
caesarakalaeii <caesarlp@gmx.de>
2023-06-23 15:49:02 +0200

    started working on html building

--- src/html_base.ts
+++ src/html_base.ts
@@ -2,39 +2,86 @@ export const htmlString = `
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Generated HTML</title>
+    <title>Thanks for watching!</title>
+    <style>
+      h1 {
+        color: blue;
+      }
+      h2 {
+        color: white;
+      }
+      .username {
+        color: purple;
+      }
+      .message {
+        color: white;
+      }
+
+      /* Flexbox styles */
+      body {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        background-color: rgba(0,0,0,0);
+      }
+      ul {
+        list-style-type: none;
+        padding: 0;
+        margin: 0;
+      }
+      li {
+        display: flex;
+        align-items: center;
+        margin-bottom: 10px;
+      }
+      li .username {
+        margin-right: 10px;
+      }
+    </style>
   </head>
   <body>
-    <h1>New Subscriptions:</h1>
+    <h2>New Subscriptions:</h2>
     <ul>
       <% newSubs.forEach(function(event) { %>
-        <li><%= event.name %> - <%= event.timestamp %></li>
+        <li>
+          <span class="username"><%= event.user %></span> -
+          <span class="message"><%= event.message %></span>
+        </li>
       <% }) %>
     </ul>
-    <h1>Current Subscribers:</h1>
+    <h2>Current Subscribers:</h2>
     <ul>
-      <% currentSubs.forEach(function(name) { %>
-        <li><%= name %></li>
+      <% currentSubs.forEach(function(user) { %>
+        <li><%= user %></li>
       <% }) %>
     </ul>
-    <h1>Cheers:</h1>
+    <h2>Cheers:</h2>
     <ul>
       <% cheers.forEach(function(event) { %>
-        <li><%= event.name %> - <%= event.timestamp %></li>
+        <li>
+          <span class="username"><%= event.user %></span> -
+          <span class="message"><%= event.message %></span>
+        </li>
       <% }) %>
     </ul>
-    <h1>Redeems:</h1>
+    <h2>Redeems:</h2>
     <ul>
       <% redeems.forEach(function(event) { %>
-        <li><%= event.name %> - <%= event.timestamp %></li>
+        <li>
+          <span class="username"><%= event.user %></span> -
+          <span class="message"><%= event.message %></span>
+        </li>
       <% }) %>
     </ul>
-    <h1>Follows:</h1>
+    <h2>Follows:</h2>
     <ul>
-      <% follows.forEach(function(name) { %>
-        <li><%= name %></li>
+      <% follows.forEach(function(user) { %>
+        <li><%= user %></li>
       <% }) %>
     </ul>
+    <h2>Sweet dreams of sour pickles</h2>
   </body>
 </html>
+
+
 `
--- src/html_builder.ts
+++ src/html_builder.ts
@@ -1,38 +1,54 @@
 import fs from 'fs';
-import { StreamEvents } from './event_collector';
+import { StreamEvents, UserEvent } from './event_collector';
+import { messages, usernames } from './test_vars';
 import { htmlString } from './html_base';
 import ejs from 'ejs';
-
+import { publicDecrypt } from 'crypto';
 
 async function saveToFile(filename: string, streamEvents: StreamEvents){
-    const html = ejs.render(htmlString, streamEvents);;
-    await fs.promises.writeFile(filename, html)
-      if (err) {
-        console.error('Error saving HTML file:', err);
-      } else {
-        console.log('HTML file saved successfully!');
-      }
+  const html = ejs.render(htmlString, streamEvents);;
+  await fs.promises.writeFile(filename, html)
+  console.log('HTML file saved successfully!');
+      
 }
-  
 
 
 // Example usage
 const events: StreamEvents = {
-  newSubs: [
-    { user: 'User1', message: "Hello from 1" },
-    { user: 'User2', message: "Hello from 1" },
-    { user: 'User3', message: "Hello from 1" },
-  ],
-  currentSubs: ['User4', 'User5', 'User6'],
-  cheers: [
-    { user: 'User7', message: "Hello from 1" },
-    { user: 'User8', message: "Hello from 1" },
-  ],
-  redeems: [
-    { user: 'User9', message: "Hello from 1" },
-    { user: 'User10', message: "Hello from 1" },
-  ],
-  follows: ['User11', 'User12', 'User13'],
+  newSubs: [],
+  currentSubs: [],
+  cheers: [],
+  redeems: [],
+  follows: [],
 };
+for(let i = 0; i<200; i++){
+  var name =  usernames[i%(usernames.length-1)]
+  if(i>60){
+    var message =  messages [i%(messages.length-1)]
+      var userEvent: UserEvent = {
+        user: name,
+        message: message
+      }
+    }
+  if(i<40){
+    events.currentSubs.push(name)
+  }
+  else if(i<80){
+    events.follows.push(name)
+  }
+  else if(i<120){
+    events.newSubs.push(userEvent)
+  }
+  else if(i<160){
+    events.cheers.push(userEvent)
+  }
+  else if(i<200){
+    events.redeems.push(userEvent)
+  }
+    
+}
+
+
+
 
 saveToFile('output.html', events).catch(err=>console.error(err));
