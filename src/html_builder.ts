f6493f39cdffd0ca65b8861d24f9d5f0401fc54a
kf <kf@4fo.de>
2023-06-23 20:43:11 +0200

    html server go brr

--- src/html_builder.ts
+++ src/html_builder.ts
@@ -3,6 +3,8 @@ import path from 'path';
 import { CheerEvent, GiftEvent, StreamEvents, UserEvent } from './event_collector';
 import { testMessages, testUsernames } from './test_vars';
 import ejs from 'ejs';
+import express from 'express';
+import { lookup } from 'mime-types';
 
 async function saveToFile(filename: string, streamEvents: StreamEvents){
   const data = await fs.promises.readFile(path.join(__dirname, "../views/credits_template.html"))
@@ -12,59 +14,77 @@ async function saveToFile(filename: string, streamEvents: StreamEvents){
       
 }
 
-
 // Example usage
-const events: StreamEvents = {
-  newSubs: [],
-  currentSubs: [],
-  gifted:[],
-  cheers: [],
-  redeems: [],
-  follows: [],
-};
-const max = 60
-for(let i = 0; i<max; i++){
-  const name =  testUsernames[i%(testUsernames.length-1)]
-  const message =  testMessages [i%(testMessages.length-1)]
-  const userEvent: UserEvent = {
-    user: name,
-    message: message
-  }
-     
-  if(i<2*max/7){
-    events.currentSubs.push(name)
-  }
-  else if(i<3*max/7){
-    events.follows.push(name)
-  }
-  else if(i<4*max/7){
-    events.newSubs.push(name)
-  }
-  else if(i<5*max/7){
-    const cheerEvent :CheerEvent ={
+function mockEvents() {
+  const events: StreamEvents = {
+    newSubs: [],
+    currentSubs: [],
+    gifted:[],
+    cheers: [],
+    redeems: [],
+    follows: [],
+  };
+  const max = 60
+  for(let i = 0; i<max; i++){
+    const name =  testUsernames[i%(testUsernames.length-1)]
+    const message =  testMessages [i%(testMessages.length-1)]
+    const userEvent: UserEvent = {
       user: name,
-      amount: Math.floor(Math.random()*100),
-      messages: [message]
+      message: message
     }
-    for (let i = 1; i< Math.floor(Math.random()*5); i++){
-      cheerEvent.messages.push(testMessages[Math.floor(Math.random()*(testMessages.length-1))])
+       
+    if(i<2*max/7){
+      events.currentSubs.push(name)
     }
-    events.cheers.push(cheerEvent)
-  }
-  else if(i<6*max/7){
-    var giftEvent: GiftEvent = {
-      user: name,
-      amount: Math.floor(Math.random()*100)
+    else if(i<3*max/7){
+      events.follows.push(name)
     }
-    events.gifted.push(giftEvent)
+    else if(i<4*max/7){
+      events.newSubs.push(name)
+    }
+    else if(i<5*max/7){
+      const cheerEvent :CheerEvent ={
+        user: name,
+        amount: Math.floor(Math.random()*100),
+        messages: [message]
+      }
+      for (let i = 1; i< Math.floor(Math.random()*5); i++){
+        cheerEvent.messages.push(testMessages[Math.floor(Math.random()*(testMessages.length-1))])
+      }
+      events.cheers.push(cheerEvent)
+    }
+    else if(i<6*max/7){
+      var giftEvent: GiftEvent = {
+        user: name,
+        amount: Math.floor(Math.random()*100)
+      }
+      events.gifted.push(giftEvent)
+    }
+    else{
+      events.redeems.push(userEvent)
+    }
   }
-  else{
-    events.redeems.push(userEvent)
-  }
-    
+
+  return events
 }
 
+async function startServer() {
+  const app = express()
 
+  app.set('view engine', 'ejs')
+  app.use('/content', express.static(path.join(__dirname, '../content'), {
+    setHeaders(res, path, stat) {
+      const mime = lookup(path)
+      if (mime) {
+        res.setHeader('content-type', mime)
+      }
+    },
+  }))
+  app.get('/mock', (req, res) => res.render('credits_template', mockEvents()))
 
+  await new Promise<void>(resolve => app.listen(3000, () => resolve()))
+}
 
-saveToFile('output.html', events).catch(err=>console.error(err));
+startServer()
+  .then(() => 'listening')
+  .catch((err) => console.error(err))
